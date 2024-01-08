e06f86903a26ce548b9688fe523957de994b9342
Luis Hernández <luhernandez@hcoadvisors.com>
2024-01-08 15:57:54 -0600

    Actualización de proyecto

--- Silifalcon.SAPConnector/SAPConnection.cs
+++ Silifalcon.SAPConnector/SAPConnection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -7,80 +8,102 @@ using SAPbobsCOM;
 
 namespace Silifalcon.SAPConnector
 {
-	public class SAPConnection : IDisposable
-	{
-		private static readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+    public class SAPConnection : IDisposable
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-		public Company Company { get; private set; }
+        public Company Company { get; private set; }
 
-		public SAPConnection()
-		{
-			if (SAPSettings.Current == null)
-			{
-				SAPException ex = new SAPException(-1, "SAPSettings.Current not defined");
-				logger.Error(ex.Message, ex);
-				Debug.WriteLine(ex.Message);
-				throw ex;
-			}
-			//Company obj = (Company)Activator.CreateInstance(Marshal.GetTypeFromCLSID(new Guid("632F4591-AA62-4219-8FB6-22BCF5F60100")));
-			Company = new Company();
-			Company.SLDServer = SAPSettings.Current.SLDServer;
-			Company.LicenseServer = SAPSettings.Current.LicenseServer;
-			Company.Server = SAPSettings.Current.Server;
-			Company.CompanyDB = SAPSettings.Current.CompanyDB;
-			Company.UserName = SAPSettings.Current.UserName;
-			Company.Password = SAPSettings.Current.Password;
-			Company.DbUserName = SAPSettings.Current.DbUserName;
-			Company.DbPassword = SAPSettings.Current.DbPassword;
-			Company.UseTrusted = SAPSettings.Current.UseTrusted;
-			Company.DbServerType = SAPSettings.Current.DbServerType;
-			//Company = obj;
-			logger.Info("Connecting");
-			Debug.WriteLine("Connecting");
-			int errCode = Company.Connect();
-			if (errCode != 0)
-			{
-				Company.GetLastError(out errCode, out var errMsg);
-				SAPConnectionException ex2 = new SAPConnectionException(errCode, errMsg);
-				logger.Error(ex2.Message, ex2);
-				Debug.WriteLine(ex2.Message);
-				throw ex2;
-			}
-		}
+        public SAPConnection()
+        {
+            GetValueConnect();
+            log.Info("Connecting to SAP.");
+            Company = new Company();
+            Company.SLDServer = SAPSettings.Current.SLDServer;
+            Company.LicenseServer = SAPSettings.Current.LicenseServer;
+            Company.Server = SAPSettings.Current.Server;
+            Company.CompanyDB = SAPSettings.Current.CompanyDB;
+            Company.UserName = SAPSettings.Current.UserName;
+            Company.Password = SAPSettings.Current.Password;
+            Company.DbUserName = SAPSettings.Current.DbUserName;
+            Company.DbPassword = SAPSettings.Current.DbPassword;
+            Company.UseTrusted = SAPSettings.Current.UseTrusted;
+            Company.DbServerType = SAPSettings.Current.DbServerType;
+            int errCode = Company.Connect();
+            CheckResponse(errCode);
+            if (errCode != 0)
+            {
+                Company.GetLastError(out errCode, out var errMsg);
+                SAPConnectionException ex2 = new SAPConnectionException(errCode, errMsg);
+                log.Error(ex2.Message, ex2);
+                Debug.WriteLine(ex2.Message);
+                throw ex2;
+            }
+            log.Info("Successful connection with SAP.");
+        }
 
-		public void CheckResponse(int response)
-		{
-			if (response == 0)
-			{
-				logger.Info("Success action");
-				Debug.WriteLine("Success action");
-				return;
-			}
-			Company.GetLastError(out response, out var errMsg);
-			SAPException ex = new SAPException(response, errMsg);
-			logger.Error(ex.Message, ex);
-			Debug.WriteLine(ex.Message);
-			throw ex;
-		}
+        private void GetValueConnect()
+        {
+            if (SAPSettings.Current == null)
+            {
+                SAPSettings.Current = new SAPSettings
+                {
+                    SLDServer = ConfigurationManager.AppSettings["SLDServer"],
+                    LicenseServer = ConfigurationManager.AppSettings["LicenseServer"],
+                    Server = ConfigurationManager.AppSettings["Server"],
+                    CompanyDB = ConfigurationManager.AppSettings["CompanyDB"],
+                    UserName = ConfigurationManager.AppSettings["UserName"],
+                    Password = ConfigurationManager.AppSettings["Password"],
+                    DbUserName = ConfigurationManager.AppSettings["DbUserName"],
+                    DbPassword = ConfigurationManager.AppSettings["DbPassword"],
+                    UseTrusted = ConfigurationManager.AppSettings["UseTrusted"].Equals("True"),
+                    DbServerType = ConfigurationManager.AppSettings["DbServerType"].Equals("MSSQL2019")
+                    ? BoDataServerTypes.dst_MSSQL2019
+                    : ConfigurationManager.AppSettings["DbServerType"].Equals("MSSQL2017")
+                    ? BoDataServerTypes.dst_MSSQL2017
+                    : ConfigurationManager.AppSettings["DbServerType"].Equals("MSSQL2016")
+                    ? BoDataServerTypes.dst_MSSQL2016
+                    : ConfigurationManager.AppSettings["DbServerType"].Equals("MSSQL2014")
+                    ? BoDataServerTypes.dst_MSSQL2014
+                    : ConfigurationManager.AppSettings["DbServerType"].Equals("MSSQL2012")
+                    ? BoDataServerTypes.dst_MSSQL2012
+                    : BoDataServerTypes.dst_HANADB
+                };
+            }
+        }
 
-		public void Dispose()
-		{
-			Dispose(x: false);
-		}
+        public void CheckResponse(int response)
+        {
+            if (response == 0)
+            {
+                log.Info("Success action");
+                Debug.WriteLine("Success action");
+                return;
+            }
+            Company.GetLastError(out response, out var errMsg);
+            SAPException ex = new SAPException(response, errMsg);
+            log.Error(ex.Message, ex);
+            throw ex;
+        }
 
-		protected virtual void Dispose(bool x)
-		{
-			if (Company.Connected)
-			{
-				logger.Info("Disconnecting");
-				Debug.WriteLine("Disconnecting");
-				Company.Disconnect();
-			}
-			GC.SuppressFinalize(this);
-			GC.Collect();
-			GC.WaitForPendingFinalizers();
-			GC.Collect();
-		}
-	}
+        public void Dispose()
+        {
+            Dispose(x: false);
+        }
+
+        protected virtual void Dispose(bool x)
+        {
+            if (Company.Connected)
+            {
+                log.Info("Disconnecting");
+                Debug.WriteLine("Disconnecting");
+                Company.Disconnect();
+            }
+            GC.SuppressFinalize(this);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
 
 }
